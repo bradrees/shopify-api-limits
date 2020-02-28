f03bbbcaa4a444356b5029c95355da7eac9c1792
Bradley Rees <github@bradleyrees.com>
2020-02-28 23:06:20 +0800

    feat (callbacks): add on_request callback

--- lib/shopify-api-throttle/shopify_api/throttle.rb
+++ lib/shopify-api-throttle/shopify_api/throttle.rb
@@ -6,25 +6,29 @@ module ShopifyAPI
       THROTTLE_MIN_CREDIT = 10
 
       def throttle(&block)
-        retried = 0
+        retried ||= 0
         begin
           if ShopifyAPI.credit_below?(THROTTLE_MIN_CREDIT)
             sleep_for = [[THROTTLE_MIN_CREDIT - ShopifyAPI.credit_left, THROTTLE_RETRY_AFTER].min, 1].max
             puts "Credit Maxed: #{ShopifyAPI.credit_left}/#{ShopifyAPI.credit_limit}, sleeping for #{sleep_for} seconds"
             sleep sleep_for
 
-            if $shopify_store and $shopify_store.respond_to? :on_throttled # Use this to call back into your application to update something
+            if $shopify_store && $shopify_store.respond_to?(:on_throttled) # Use this to call back into your application when throttled
               $shopify_store.on_throttled
             end
           end
 
+          if $shopify_store && $shopify_store.respond_to?(:on_request) # Use this to call back into your application when a request happens
+            $shopify_store.on_request
+          end
+
           yield
         rescue ActiveResource::ResourceNotFound, ActiveResource::BadRequest, ActiveResource::UnauthorizedAccess,
-               ActiveResource::ForbiddenAccess, ActiveResource::MethodNotAllowed, ActiveResource::ResourceGone,
-               ActiveResource::ResourceConflict, ActiveResource::ResourceInvalid => ex
+            ActiveResource::ForbiddenAccess, ActiveResource::MethodNotAllowed, ActiveResource::ResourceGone,
+            ActiveResource::ResourceConflict, ActiveResource::ResourceInvalid => ex
           raise ex
         rescue ActiveResource::ConnectionError, ActiveResource::ServerError,
-               ActiveResource::ClientError, Timeout::Error, OpenSSL::SSL::SSLError => ex
+            ActiveResource::ClientError, Timeout::Error, OpenSSL::SSL::SSLError => ex
           if retried <= THROTTLE_RETRY_MAX
             retry_after = ((ex.respond_to?(:response) && ex.response && ex.response['Retry-After']) || THROTTLE_RETRY_AFTER).to_i
             puts "Throttle Retry: #{ShopifyAPI.credit_left}/#{ShopifyAPI.credit_limit}, sleeping for #{retry_after} seconds"
